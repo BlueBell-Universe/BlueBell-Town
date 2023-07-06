7dbb6c7120ed074567cf658f4a6cbe895f68a630
지훈 <80836736+rlawlgns3433@users.noreply.github.com>
2023-07-07 01:33:09 +0900

    움직임 추가 및 플랫폼 변경

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -6,11 +6,16 @@ public class PlayerMove : MonoBehaviour
     public float speed;
     private Joystick joystick;
     private SpriteRenderer spriteRenderer;
+    private Rigidbody2D rigid;
+    private bool isJumping;
+    [SerializeField]
+    private float jumpForce;
 
     void Awake()
     {
+        jumpForce = 3.0f;
         animator = GetComponent<Animator>();
-
+        rigid = GetComponent<Rigidbody2D>();
         joystick = GameObject.FindObjectOfType<Joystick>();
     }
     void FixedUpdate()
@@ -30,11 +35,29 @@ public class PlayerMove : MonoBehaviour
         else  animator.SetBool("isRun", false);
     }
 
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.CompareTag("Ground"))
+        {
+            isJumping = false;
+            animator.SetBool("isJump", false);
+        }
+    }
     private void MoveControl()
     {
         animator.SetBool("isRun", true);
-
         Vector3 rightMovement = Vector3.right * speed * Time.deltaTime * joystick.Horizontal;
         transform.position += rightMovement;
+    }
+
+    public void Jump()
+    {
+        if (!isJumping)
+        {
+            animator.SetBool("isJump", true);
+            rigid.AddForce(Vector3.up * jumpForce, ForceMode2D.Impulse);
+            isJumping = true;
+        }
+        else return;
     }
 }
