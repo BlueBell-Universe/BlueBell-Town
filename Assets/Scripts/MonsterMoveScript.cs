727d052dbfba5bb48e9f5d9adfdfc24e914743e0
yujin <yujin2625@gmail.com>
2023-07-13 02:23:56 +0900

    monster attack script added

--- Assets/Scripts/MonsterMoveScript.cs
+++ Assets/Scripts/MonsterMoveScript.cs
@@ -4,35 +4,37 @@ using UnityEngine;
 
 public class MonsterMoveScript : MonoBehaviour
 {
+    [Header("Move")]
     RaycastHit2D hitBtm, hitFt; //hitBottom, hitFront
     SpriteRenderer spriteRenderer;
     float rayBtmLength = 0.5f;
     float rayFtLength = 0.05f;
     float moveSpeed = 1f;
     float flipX = 1f;
+
+    [Header("Attack")]
+    MonsterAttackScript monsterAttackScript;
+    RaycastHit2D hitPlayer;
+    public float rayPlayerLength = 0.5f;
+
     private void Start()
     {
         spriteRenderer = gameObject.GetComponentInChildren<SpriteRenderer>();
+        monsterAttackScript = GetComponent<MonsterAttackScript>();
     }
     private void Update()
     {
         Debug.Log("ground : " + CheckGround());
         Debug.Log("front : " + CheckFront());
-        if (hitBtm && !hitFt)
+        if (CheckGround() && !CheckFront())
         {
             MonsterMove();
         }
-        else if ((!hitBtm || hitFt)|| (hitBtm && hitFt))
+        else if ((!CheckGround() || CheckFront())|| (CheckGround() && CheckFront()))
         {
             flipX *= -1;
             spriteRenderer.flipX =! spriteRenderer.flipX;
         }
-        //else if (hitBtm && hitFt)
-        //{
-        //    flipX *= -1;
-        //    spriteRenderer.flipX = !spriteRenderer.flipX;
-
-        //}
     }
     public bool CheckGround()
     {
@@ -68,4 +70,17 @@ public class MonsterMoveScript : MonoBehaviour
         transform.position = new Vector2(transform.position.x + flipX * moveSpeed * Time.deltaTime, transform.position.y);
         //µÙ∑π¿Ã
     }
+
+    public void CheckPlayer()
+    {
+        Vector2 startRay = new Vector2(transform.position.x + flipX * 0.3f, transform.position.y - 0.25f);
+        Debug.DrawRay(startRay, -transform.up * rayPlayerLength, Color.red);
+        hitPlayer = Physics2D.Raycast(startRay, -transform.up, rayPlayerLength);
+        if (hitPlayer.transform.CompareTag("Player"))
+        {
+            //follow player
+
+        }
+    }
+
 }
