19fa2680f18cca6c2debba6fe9919c029accac6f
yujin <yujin2625@gmail.com>
2023-07-23 05:29:10 +0900

    monster hit

--- Assets/Scripts/FireObject.cs
+++ Assets/Scripts/FireObject.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class FireObject : MonoBehaviour
 {
+    public float damage = 5;
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if(collision.gameObject.CompareTag("Monster"))
---
+++ Assets/Scripts/MonsterStat.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MonsterStat : MonoBehaviour
+{
+    [SerializeField] private float hp = 100;
+    public float currentHp;
+    [SerializeField] private float atk = 10;
+    [SerializeField] private float del;
+    [SerializeField] private float exp;
+
+    public GameObject HpBarBackground;
+    public Image HpBarFilled;
+    void Start()
+    {
+        currentHp = hp;
+        HpBarFilled.fillAmount = 1f;
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        GameObject collObj = collision.gameObject;
+        if (collObj.CompareTag("SkillObj"))
+        {
+            float hit = collObj.GetComponent<FireObject>().damage;
+            Hit(hit);
+        }
+    }
+    public void Hit(float _hit)
+    {
+        if (!HpBarBackground.activeInHierarchy)
+        {
+            HpBarBackground.SetActive(true);
+        }
+        currentHp -= _hit;
+        HpBarFilled.fillAmount = currentHp / hp;
+    }
+}
