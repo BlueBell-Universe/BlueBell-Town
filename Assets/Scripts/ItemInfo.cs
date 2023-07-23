3e97e526f320462dfa74b6738af50cf5f686a392
yujin <yujin2625@gmail.com>
2023-07-23 16:29:33 +0900

    update item max amount

--- Assets/Scripts/ItemInfo.cs
+++ Assets/Scripts/ItemInfo.cs
@@ -8,6 +8,7 @@ public class ItemInfo
     public string itemName;
     public string itemCode;
     public string itemType;
+    private int maxAmount;
     public int itemAmount;
     public ItemInfo(GameObject _itemObj,string _itemName, string _itemCode)
     {
@@ -21,12 +22,15 @@ public class ItemInfo
         switch (itemCode.Substring(0, itemCode.IndexOf("_")))
         {
             case "1":
+                maxAmount = 10;
                 itemType = "코인";
                 break;
             case "2":
+                maxAmount = 1;
                 itemType = "퀘스트아이템";
                 break;
             case null:
+                maxAmount = 1;
                 itemType = "기타 아이템";
                 break;
         }
@@ -35,7 +39,7 @@ public class ItemInfo
 
     public int SetItemAmount()
     {
-        itemAmount = Random.Range(1, 10);
+        itemAmount = Random.Range(1, maxAmount);
 
         return itemAmount;
     }
