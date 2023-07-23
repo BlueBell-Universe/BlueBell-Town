5de88f65851d8ee14fff1c412e6d0ac70817aa21
yujin <yujin2625@gmail.com>
2023-07-23 16:09:30 +0900

    item info, destroy added

--- Assets/Scripts/DropItemScript.cs
+++ Assets/Scripts/DropItemScript.cs
@@ -4,6 +4,19 @@ using UnityEngine;
 
 public class DropItemScript : MonoBehaviour
 {
+    public ItemInfo itemInfo;
+    void Start()
+    {
+        itemInfo = new ItemInfo(gameObject, gameObject.name, gameObject.tag);
+    }
 
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.transform.CompareTag("Player"))
+        {
+            Debug.Log("플레이어가 " + itemInfo.checkItemType() + "종류의 아이템 " + name.Substring(0,name.IndexOf("("))+"을/를 획득했습니다.");
+            Destroy(gameObject);
+        }
+    }
 
 }
---
+++ Assets/Scripts/ItemInfo.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemInfo 
+{
+    public GameObject itemObj;
+    public string itemName;
+    public string itemCode;
+    public string itemType;
+    public ItemInfo(GameObject _itemObj,string _itemName, string _itemCode)
+    {
+        itemObj = _itemObj;
+        itemName = _itemName;
+        itemCode = _itemCode;
+    }
+
+    public string checkItemType()
+    {
+        switch (itemCode.Substring(0, itemCode.IndexOf("_")))
+        {
+            case "1":
+                itemType = "코인";
+                break;
+            case "2":
+                itemType = "퀘스트아이템";
+                break;
+            case null:
+                itemType = "기타 아이템";
+                break;
+        }
+        return itemType;
+    }
+
+
+}
