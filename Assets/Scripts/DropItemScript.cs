3a90e1ca1144f81ac49359f8dd5ad8f96093b4f5
yujin <yujin2625@gmail.com>
2023-08-04 02:33:10 +0900

    updated drop item to trigger

--- Assets/Scripts/DropItemScript.cs
+++ Assets/Scripts/DropItemScript.cs
@@ -5,20 +5,27 @@ using UnityEngine;
 public class DropItemScript : MonoBehaviour
 {
     public ItemInfo itemInfo;
+    public bool triggering = false;
     void Start()
     {
         itemInfo = new ItemInfo(gameObject, gameObject.name, gameObject.tag);
         itemInfo.SetItemAmount();
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
+
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.transform.CompareTag("Player"))
+        if (!triggering)
         {
-            Debug.Log(name.Substring(0,name.IndexOf("(")) + ":" + itemInfo.itemAmount);
-            BackendGameData.Instance.GamedataUpdate(itemInfo.itemAmount);
-            Destroy(gameObject);
+            triggering = true;
+            if (collision.transform.CompareTag("Player"))
+            {
+                Debug.Log(name.Substring(0, name.IndexOf("(")) + ":" + itemInfo.itemAmount);
+                BackendGameData.Instance.GamedataUpdate(itemInfo.itemAmount);
+                Destroy(gameObject);
+            }
         }
     }
+
 
 }
