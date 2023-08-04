2ab78b57e8b0e78c306e4fa1c97c19f89d9e6c4c
지훈 <80836736+rlawlgns3433@users.noreply.github.com>
2023-08-04 16:40:41 +0900

    인벤토리 UI

---
+++ Assets/Scripts/JIhunLab_Inventory/Inventory.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    public List<Item> items;
+
+    [SerializeField]
+    private Transform slotParent;
+    [SerializeField]
+    private Slot[] slots;
+    [SerializeField]
+    private bool active;
+
+    private void OnValidate()
+    {
+        slots = slotParent.GetComponentsInChildren<Slot>();
+    }
+
+    void Awake()
+    {
+        FreshSlot();
+    }
+
+
+    public void FreshSlot()
+    {
+        int i = 0;
+        for (; i < items.Count && i < slots.Length; i++)
+        {
+            slots[i].item = items[i];
+        }
+        for (; i < slots.Length; i++)
+        {
+            slots[i].item = null;
+        }
+    }
+
+    public void AddItem(Item _item)
+    {
+        if (items.Count < slots.Length)
+        {
+            items.Add(_item);
+            FreshSlot();
+        }
+        else
+        {
+            print("½½·ÔÀÌ °¡µæ Â÷ ÀÖ½À´Ï´Ù.");
+        }
+    }
+
+    public void OpenInventory()
+    {
+        gameObject.SetActive(true);
+    }
+
+    public void CloseInventory()
+    {
+        gameObject.SetActive(false);
+    }
+}
